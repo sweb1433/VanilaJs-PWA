3398e3724848dd12e97cd826aa9f6f68a9f1cd3c
sweb1433 <sweb1433@gmail.com>
2020-08-01 18:12:51 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,86 @@
+const container = document.querySelector(".container");
+const coffees = [
+  {
+    name: "Perspiciatis",
+    image: "images/coffee1.jpg"
+  },
+  {
+    name: "Voluptatem",
+    image: "images/coffee2.jpg"
+  },
+  {
+    name: "Explicabo",
+    image: "images/coffee3.jpg"
+  },
+  {
+    name: "Rchitecto",
+    image: "images/coffee4.jpg"
+  },
+  {
+    name: " Beatae",
+    image: "images/coffee5.jpg"
+  },
+  {
+    name: " Vitae",
+    image: "images/coffee6.jpg"
+  },
+  {
+    name: "Inventore",
+    image: "images/coffee7.jpg"
+  },
+  {
+    name: "Veritatis",
+    image: "images/coffee8.jpg"
+  },
+  {
+    name: "Accusantium",
+    image: "images/coffee9.jpg"
+  }
+];
+const showCoffees = () => {
+  let output = "";
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `)
+  );
+  container.innerHTML = output;
+};
+
+document.addEventListener("DOMContentLoaded", showCoffees);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
+
+function displayNotification() {
+  console.log("Calling push")
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      var options = {
+        body: 'Here is a notification body!',
+        icon: 'images/coffee1.jpg',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        }
+      };
+      reg.showNotification('Hello world!', options);
+    });
+  }
+}
+displayNotification()
+
+setInterval(displayNotification,30000)
+
